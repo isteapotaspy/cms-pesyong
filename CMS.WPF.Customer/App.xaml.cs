dc1d92b053703b03a77c941067f047698836bef4
Jane <jennmiku@gmail.com>
2026-03-10 09:24:01 +0800

    Add project files.

---
+++ CMS.WPF.Customer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CMS.WPF.Customer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
