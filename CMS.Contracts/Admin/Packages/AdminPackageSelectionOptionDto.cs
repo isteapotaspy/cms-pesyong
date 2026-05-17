2eb289dc209bf6dfb9d9e6d7988c4d11da2d739a
Jane <jennmiku@gmail.com>
2026-05-18 00:41:42 +0800

    Add AdminDtos in CMS.Contracts

---
+++ CMS.Contracts/Admin/Packages/AdminPackageSelectionOptionDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMS.Contracts.Admin.Packages;
+public class AdminPackageSelectionOptionDto
+{
+    public int Id { get; set; }
+    public int MealId { get; set; }
+    public string MealName { get; set; } = string.Empty;
+    public string MealType { get; set; } = string.Empty;
+    public decimal AdditionalPrice { get; set; }
+    public bool IsDefault { get; set; }
+}
